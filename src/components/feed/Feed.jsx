c6bb5459f1fda24c4f318a22d56621ef194a825a
milleseseau <leojimlavi@gmail.com>
2021-11-11 23:14:54 -0500

    Feed (Share, Post Components)

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,7 +1,21 @@
+import Post from "../post/Post";
+import Share from "../share/Share";
 import "./feed.css";
 
 const Feed = () => {
-	return <div className="feed">feed</div>;
+	return (
+		<div className="feed">
+			<div className="feedWrapper">
+				<Share />
+				<Post />
+				<Post />
+				<Post />
+				<Post />
+				<Post />
+				<Post />
+			</div>
+		</div>
+	);
 };
 
 export default Feed;
