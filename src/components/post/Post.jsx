e18a67276b530db94381668d4659b5e4e285c6c7
milleseseau <leojimlavi@gmail.com>
2021-11-12 00:38:11 -0500

    using dynamic data

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,7 +1,8 @@
 import "./post.css";
 import { MoreVert } from "@material-ui/icons";
+import { Users } from "../../dummyData";
 
-const Post = () => {
+const Post = ({ post }) => {
 	return (
 		<div className="post">
 			<div className="postWrapper">
@@ -9,24 +10,24 @@ const Post = () => {
 					<div className="postTopLeft">
 						<img className="postProfileImg" src="assets/person/1.jpeg" alt="" />
 						<span className="postUsername">Mille Seseau</span>
-						<span className="postDate">5 mins ago</span>
+						<span className="postDate">{post.date}</span>
 					</div>
 					<div className="postTopRight">
 						<MoreVert />
 					</div>
 				</div>
 				<div className="postCenter">
-					<span className="postText">Hey! it's my first post:)</span>
-					<img src="assets/post/1.jpeg" alt="" className="postImg" />
+					<span className="postText">{post?.desc}</span>
+					<img src={post.photo} alt="" className="postImg" />
 				</div>
 				<div className="postBottom">
 					<div className="postBottomLeft">
 						<img className="likeIcon" src="assets/like.png" alt="" />
 						<img className="likeIcon" src="assets/heart.png" alt="" />
-						<span className="postLikeCounter">32 people like it</span>
+						<span className="postLikeCounter">{post.like} people like it</span>
 					</div>
 					<div className="postBottomRight">
-						<span className="postCommentText">9 comments</span>
+						<span className="postCommentText">{post.comment} comments</span>
 					</div>
 				</div>
 			</div>
--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -6,12 +6,7 @@ const Share = () => {
 		<div className="share">
 			<div className="shareWrapper">
 				<div className="shareTop">
-					<img
-						src=""
-						alt=""
-						className="shareProfileImg"
-						src="/assets/person/1.jpeg"
-					/>
+					<img alt="" className="shareProfileImg" src="/assets/person/1.jpeg" />
 					<input
 						placeholder="What's in your mind Safak?"
 						className="shareInput"
