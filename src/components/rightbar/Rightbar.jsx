4d16dab6a0431e4c56194062c80ede0246abfe0d
milleseseau <leojimlavi@gmail.com>
2021-11-16 23:09:52 -0500

    login, register

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -2,10 +2,10 @@ import "./rightbar.css";
 import { Users } from "../../dummyData";
 import Online from "../online/Online";
 
-const Rightbar = () => {
-	return (
-		<div className="rightbar">
-			<div className="rightbarWrapper">
+const Rightbar = ({ profile }) => {
+	const HomeRightbar = () => {
+		return (
+			<>
 				<div className="birthdayContainer">
 					<img src="assets/gift.png" alt="" className="birthdayImg" />
 					<span className="birthdayText">
@@ -19,6 +19,87 @@ const Rightbar = () => {
 						return <Online key={u.id} user={u} />;
 					})}
 				</ul>
+			</>
+		);
+	};
+
+	const ProfileRightbar = () => {
+		return (
+			<>
+				<h4 className="rightbarTitle">User information</h4>
+				<div className="rightbarInfo">
+					<div className="rightbarInfoItem">
+						<span className="rightbarInfoKey">City:</span>
+						<div className="rightbarInfoValue">New York</div>
+					</div>
+					<div className="rightbarInfoItem">
+						<span className="rightbarInfoKey">From:</span>
+						<div className="rightbarInfoValue">Madrid</div>
+					</div>
+					<div className="rightbarInfoItem">
+						<span className="rightbarInfoKey">Relationship:</span>
+						<div className="rightbarInfoValue">Single</div>
+					</div>
+				</div>
+				<h4 className="rightbarTitle">User friends</h4>
+				<div className="rightbarFollowings">
+					<div className="rightbarFollowing">
+						<img
+							src="assets/person/1.jpeg"
+							alt=""
+							className="rightbarFollowingImg"
+						/>
+						<span className="rightbarFollowingName">John Carter</span>
+					</div>
+					<div className="rightbarFollowing">
+						<img
+							src="assets/person/2.jpeg"
+							alt=""
+							className="rightbarFollowingImg"
+						/>
+						<span className="rightbarFollowingName">John Carter</span>
+					</div>
+					<div className="rightbarFollowing">
+						<img
+							src="assets/person/3.jpeg"
+							alt=""
+							className="rightbarFollowingImg"
+						/>
+						<span className="rightbarFollowingName">John Carter</span>
+					</div>
+					<div className="rightbarFollowing">
+						<img
+							src="assets/person/4.jpeg"
+							alt=""
+							className="rightbarFollowingImg"
+						/>
+						<span className="rightbarFollowingName">John Carter</span>
+					</div>
+					<div className="rightbarFollowing">
+						<img
+							src="assets/person/5.jpeg"
+							alt=""
+							className="rightbarFollowingImg"
+						/>
+						<span className="rightbarFollowingName">John Carter</span>
+					</div>
+					<div className="rightbarFollowing">
+						<img
+							src="assets/person/6.jpeg"
+							alt=""
+							className="rightbarFollowingImg"
+						/>
+						<span className="rightbarFollowingName">John Carter</span>
+					</div>
+				</div>
+			</>
+		);
+	};
+
+	return (
+		<div className="rightbar">
+			<div className="rightbarWrapper">
+				{profile ? <ProfileRightbar /> : <HomeRightbar />}
 			</div>
 		</div>
 	);
--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -11,10 +11,27 @@ const Profile = () => {
 			<div className="profile">
 				<Sidebar />
 				<div className="profileRight">
-					<div className="profileRightTop">qowkdqd</div>
+					<div className="profileRightTop">
+						<div className="profileCover">
+							<img
+								src="assets/post/3.jpeg"
+								alt=""
+								className="profileCoverImg"
+							/>
+							<img
+								src="assets/person/7.jpeg"
+								alt=""
+								className="profileUserImg"
+							/>
+						</div>
+					</div>
+					<div className="profileInfo">
+						<h4 className="profileInfoName">Mille Seseau</h4>
+						<span className="profileInfoDesc">Hello my friends!</span>
+					</div>
 					<div className="profileRightBottom">
 						<Feed />
-						<Rightbar />
+						<Rightbar profile />
 					</div>
 				</div>
 			</div>
